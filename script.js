3ad013f61a1123ad3c05414e413aa7f1096e2608
Sviat <svyatoslav496@gmail.com>
2020-04-30 17:03:53 +0300

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ decr = 10;
 
 console.log(++incr);
 console.log(--decr);
+
+const addEvent = document.querySelectorAll('div');
+function newEvent() {
+  addEvent.addEventListener('click');
+}
+newEvent();
