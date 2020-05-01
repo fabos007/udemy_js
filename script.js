128fa6524224fdda0985ece72be1cd1318b47558
Sviat <svyatoslav496@gmail.com>
2020-05-01 23:11:25 +0300

    task 3. Created 3 new functions detectDayBudget(),  (output day budget), detectLevel() Level of salary, chooseOptExpenses, ask for optional expenses and push to appData

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let money = prompt('Your budget for this month?');
-let time = prompt('Enter date in the following formate YYYY-MM-DD', '2020-04-30');
+let money, time;
+function start () {
+  money = prompt('Your budget for this month?', '');
+  time = prompt('Enter date in the following formate YYYY-MM-DD', '2020-04-30');
+
+  while (isNaN(money) || money == '' || money == null) {
+    money = prompt('Your budget for this month?', '');
+  }
+}
+start();
 
 
 
@@ -13,64 +21,72 @@ let appData = {
 
   },
   income: [],
-  savings: false
+  savings: true
 };
 
-
-// for(let i = 0; i < 2; i++){
-// let a = prompt('What is the mandatory expense this month?'),
-//     b = prompt('What is the amount?'); 
-
-//   if ((typeof(a)!= null) && (typeof(b)!= null) && a != '' && b != '' && a.length < 50 && b.length < 10)  {
-//     console.log('Done');
-//   appData.expenses[a] = b;
-
-//   }else {
-//     console.log('bad result');
-//     i--;
-//   }
-// }
-
-// let i = 0;
-//   while (i < 2) {
-//     let a = prompt('What is the mandatory expense this month?'),
-//       b = prompt('What is the amount?'); 
-
-//     if ((typeof(a)!= null) && (typeof(b)!= null) && a != '' && b != '' && a.length < 50 && b.length < 10)  {
-//       console.log('Done');
-//     appData.expenses[a] = b;
-//   }else {
-//     console.log('bad result');
-//     i--;
-//   }
-//   i++;
-// }
-
-// let i = 0;
-// do {
-//   let a = prompt('What is the mandatory expense this month?'),
-//         b = prompt('What is the amount?'); 
+function chooseExpenses () {
   
-//       if ((typeof(a)!= null) && (typeof(b)!= null) && a != '' && b != '' && a.length < 50 && b.length < 10)  {
-//         console.log('Done');
-//       appData.expenses[a] = b;
-//     }else {
-//       console.log('bad result');
-//       i--;
-//     }
-//     i++;
-// } while (i < 2);
-
-
-appData.moneyPerDay = appData.budget/30;
-alert('Everyday budget is' +  appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-  console.log("Min level");
-}else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
-  console.log('Middle level');
-}else if (appData.moneyPerDay > 2000){
-  console.log('high level');
-}else {
-  console.log('invalid ipnput');
+for(let i = 0; i < 2; i++){
+  let a = prompt('What is the mandatory expense this month?'),
+      b = prompt('What is the amount?'); 
+  
+    if ((typeof(a)!= null) && (typeof(b)!= null) && a != '' && b != '' && a.length < 50 && b.length < 10)  {
+      console.log('Done');
+    appData.expenses[a] = b;
+  
+    }else {
+      console.log('bad result');
+      i--;
+    }
+  }
 }
+
+chooseExpenses();
+
+function detectDayBudget () {
+  appData.moneyPerDay = (appData.budget/30).toFixed();
+  alert('Everyday budget is ' +  appData.moneyPerDay);
+
+}
+detectDayBudget();
+
+function detectLevel (){
+  if(appData.moneyPerDay < 100) {
+    console.log("Min level");
+  }else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
+    console.log('Middle level');
+  }else if (appData.moneyPerDay > 2000){
+    console.log('high level');
+  }else {
+    console.log('invalid ipnput');
+  }
+}
+detectLevel();
+
+function chooseOptExpenses (){
+  let optExpenses, optAmount;
+  for (let i = 0; i < 3; i++){
+    optExpenses = prompt ('Enter optional expenses', '');
+    optAmount = prompt ('Enter amount', '');
+    if ((typeof(optExpenses)!= null) && (typeof(optAmount)!= null) && optExpenses != '' && optAmount != '' && optExpenses.length < 50 && optAmount.length < 10){
+      appData.optionalExpenses[optExpenses] = optAmount;
+    }else {
+      console.log('bad result');
+      i--;
+
+    }
+  }
+}
+chooseOptExpenses();
+
+function checkSavings () {
+  if (appData.savings) {
+    let save =+prompt('What is the amount of savings?'),
+      percent = +prompt('What is the percent of deposit');
+
+      appData.monthIncome = save/100/12*percent;
+      alert("Income from your deposit: " + appData.monthIncome);
+  }
+}
+
+checkSavings();
