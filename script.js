b0fc204cb04c024ad456cc0ab4596fe9d6c318b9
Sviat <svyatoslav496@gmail.com>
2020-05-02 12:26:41 +0300

    task 4 (first comm)

--- script.js
+++ script.js
@@ -21,67 +21,55 @@ let appData = {
 
   },
   income: [],
-  savings: true
-};
-
-function chooseExpenses () {
-  
-for(let i = 0; i < 2; i++){
-  let a = prompt('What is the mandatory expense this month?'),
-      b = prompt('What is the amount?'); 
-  
-    if ((typeof(a)!= null) && (typeof(b)!= null) && a != '' && b != '' && a.length < 50 && b.length < 10)  {
-      console.log('Done');
-    appData.expenses[a] = b;
-  
+  savings: true,
+  chooseExpenses: function(){
+    for(let i = 0; i < 2; i++){
+      let a = prompt('What is the mandatory expense this month?'),
+          b = prompt('What is the amount?'); 
+      
+        if ((typeof(a)!= null) && (typeof(b)!= null) && a != '' && b != '' && a.length < 50 && b.length < 10)  {
+          console.log('Done');
+        appData.expenses[a] = b;
+      
+        }else {
+          console.log('bad result');
+          i--;
+        }
+      }
+  },
+  detectDayBudget: function(){
+    appData.moneyPerDay = (appData.budget/30).toFixed();
+    alert('Everyday budget is ' +  appData.moneyPerDay);
+  },
+  detectLevel: function(){
+    if(appData.moneyPerDay < 100) {
+      console.log("Min level");
+    }else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
+      console.log('Middle level');
+    }else if (appData.moneyPerDay > 2000){
+      console.log('high level');
     }else {
-      console.log('bad result');
-      i--;
+      console.log('invalid ipnput');
     }
-  }
-}
-
-chooseExpenses();
-
-function detectDayBudget () {
-  appData.moneyPerDay = (appData.budget/30).toFixed();
-  alert('Everyday budget is ' +  appData.moneyPerDay);
-
-}
-detectDayBudget();
-
-function detectLevel (){
-  if(appData.moneyPerDay < 100) {
-    console.log("Min level");
-  }else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
-    console.log('Middle level');
-  }else if (appData.moneyPerDay > 2000){
-    console.log('high level');
-  }else {
-    console.log('invalid ipnput');
-  }
-}
-detectLevel();
-
-function chooseOptExpenses (){
-  let optExpenses;
-  for (let i = 0; i < 3; i++){
-    optExpenses = prompt ('Enter optional expense', '');
-    appData.optionalExpenses[i] = optExpenses;
-
-    }
+  },
+  checkSavings: function(){
+    if (appData.savings) {
+      let save =+prompt('What is the amount of savings?'),
+        percent = +prompt('What is the percent of deposit');
   
-}
-chooseOptExpenses();
-
-function checkSavings () {
-  if (appData.savings) {
-    let save =+prompt('What is the amount of savings?'),
-      percent = +prompt('What is the percent of deposit');
-
-      appData.monthIncome = save/100/12*percent;
-      alert("Income from your deposit: " + appData.monthIncome);
+        appData.monthIncome = save/100/12*percent;
+        alert("Income from your deposit: " + appData.monthIncome);
+    }
+  },
+  chooseOptExpenses: function(){
+    let optExpenses;
+    for (let i = 0; i < 3; i++){
+      optExpenses = prompt ('Enter optional expense', '');
+      appData.optionalExpenses[i] = optExpenses;
+  
+      }
+  },
+  chooseIncome: function(){
+    
   }
-}
-
-checkSavings();
+};
